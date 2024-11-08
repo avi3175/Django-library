525627e3e26b55f4e30264d2abc827604935ba3a
avi3175 <avishahriar@.com>
2024-11-08 19:03:45 +0600

    4th commit

--- library_system/settings.py
+++ library_system/settings.py
@@ -142,8 +142,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'avishahriar@gmail.com'
-EMAIL_HOST_PASSWORD = 'zqpb kzsg pgzu gjyn'
 
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
