a50a32458898cdc8bcb6af0d574fa9026ceb13ef
avi3175 <avishahriar@.com>
2024-11-08 20:44:19 +0600

    done

--- library_system/settings.py
+++ library_system/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=1@oy@b5*zf*i1fhntm971=#u=#*m_1f=z--jrt!_3aivbam%@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -131,7 +131,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/ 'media'
 
 
-import os
+
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
